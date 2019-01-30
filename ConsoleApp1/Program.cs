43e73b01e80657360ce5fda65a0feba9eaf9e9ff
visualstudio <visualstudio@122-PC10>
2019-01-30 11:35:02 -0500

    Countryside

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,18 +6,41 @@ using System.Threading.Tasks;
 
 namespace c0730283
 {
-      class Program
+    class Program
     {
         static void Main(string[] args)
         {
             //TODD
         }
     }
-      class village
+    class village
     {
         public village nextvillage;
+        public village previousvillage;
         public string villageName;
         public bool isAstrildeHere = false;
-        
+
+    }
+    class Countryside
+    {
+        village Maple = new village();
+        village Toronto = new village();
+        village Ajax = new village();
+        village First;
+        village Last;
+
+        public void Launch()
+        {
+            Maple.villageName = "Maple";
+            Maple.nextvillage = Toronto;
+            Maple.previousvillage = null;
+            Toronto.villageName = "Toronto";
+            Toronto.nextvillage = Ajax;
+            Toronto.previousvillage = Maple;
+            Ajax.villageName = "Ajax";
+            Ajax.nextvillage = null;
+            Ajax.nextvillage = Toronto;
+        }
     }
 }
+
